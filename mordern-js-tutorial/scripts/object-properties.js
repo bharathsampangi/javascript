90ecd08e1955ca245121d6ef54304240c7df09a0
bharathsampangi <bharathsampangi95@gmail.com>
2024-10-07 20:41:16 +0530

    object properties

--- mordern-js-tutorial/scripts/object-properties.js
+++ mordern-js-tutorial/scripts/object-properties.js
@@ -317,3 +317,89 @@
 //         alert(`Inherited ${prop}`)
 //     }
 // }
+
+// let animal = {
+//     jumps: null
+// };
+
+// let rabbit = {
+//     __proto__: animal,
+//     jumps: true
+// }
+
+// alert(rabbit.jumps)
+
+// delete rabbit.jumps
+
+// alert(rabbit.jumps)
+
+// delete animal.jumps
+
+// alert(rabbit.jumps)
+
+// let head = {
+//     glasses: 1
+// }
+
+// let table = {
+//     pen: 3,
+//     __proto__: head
+// }
+
+// let bed = {
+//     sheet: 1,
+//     pillow: 2,
+//     __proto__: table
+// }
+
+// let pockets = {
+//     money: 2000,
+//     __proto__: bed
+// }
+
+// alert(pockets.pen)
+// alert(bed.glasses)
+
+// let hamster = {
+//     eat(food) {
+//         this.stomach.push(food)
+//     }
+// }
+
+// let speedy = {
+//     stomach: [],
+//     __proto__: hamster
+// }
+
+// let lazy = {
+//     stomach: [],
+//     __proto__: hamster
+// }
+
+// speedy.eat('apple')
+
+// alert(speedy.stomach)
+
+// alert(lazy.stomach)
+
+let hamster = {
+    stomach: [],
+
+    eat(food) {
+        this.stomach = [food]
+    }
+}
+
+let speedy = {
+    __proto__: hamster
+}
+
+let lazy = {
+    __proto__: hamster
+}
+
+speedy.eat('apple')
+
+alert(speedy.stomach)
+
+alert(lazy.stomach)
